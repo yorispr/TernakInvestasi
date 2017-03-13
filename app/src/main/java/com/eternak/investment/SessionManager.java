dcb7cace01ae991c9adcb12e84cb15f7b89802f1
YORIS <yorisprayogo@gmail.com>
2017-03-13 17:35:35 +0700

    a

--- app/src/main/java/com/eternak/investment/SessionManager.java
+++ app/src/main/java/com/eternak/investment/SessionManager.java
@@ -70,7 +70,6 @@ public class SessionManager {
         editor.commit();
     }
 
-
     /**
      * Check login method wil check user login status
      * If false it will redirect user to login page
